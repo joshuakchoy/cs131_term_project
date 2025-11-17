70a9f3c02e2608149c29e0607ebc0a8d25ca961f
JanDavidElla <jd.ella@yahoo.com>
2025-11-16 21:11:15 -0800

    Deleted and modified some files

--- cs131_term_project-1/app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
-
-class LoginForm(FlaskForm):
-    email = EmailField("Email", validators=[DataRequired(), EMail()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
-    submit = SubmitField("Log In")
