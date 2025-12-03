ece458f1f444955b9e3b884194e58b5c4bff1ea0
JanDavidElla <jd.ella@yahoo.com>
2025-12-03 00:24:14 -0800

    Bug fixes on authentication

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_login import login_required
 import os
 from ..forms import CreateAssignmentForm
 from ..models import db, Assignment
@@ -21,15 +22,18 @@ def grades():
     return render_template("main/grades.html")
 
 @bp.route("/classes")
+@login_required
 def classes():
     return render_template("main/classes.html")
 
 @bp.route("/assignments")
+@login_required
 def assignments():
     assignments = Assignment.query.order_by(Assignment.id.desc()).all()
     return render_template("main/assignments.html", assignments=assignments)
 
 @bp.route("/create_assignment", methods=["GET", "POST"])
+@login_required
 def create_assignment():
     form = CreateAssignmentForm()
     if form.validate_on_submit():
@@ -45,6 +49,7 @@ def create_assignment():
     return render_template("main/create_assignment.html", form=form)
 
 @bp.route("/analytics")
+@login_required
 def analytics():
     return render_template("main/analytics.html")
 
